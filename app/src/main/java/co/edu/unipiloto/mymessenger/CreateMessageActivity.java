01cfc1d61850656d29b3caa7fc27add3912bfa8f
Diestross <carvajaledwar0@gmail.com>
2024-09-11 13:16:11 -0500

    Nuevo proyecto

---
+++ app/src/main/java/co/edu/unipiloto/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,31 @@
+package co.edu.unipiloto.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CreateMessageActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
+    }
+
+    public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,33 @@
+package co.edu.unipiloto.mymessenger;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.mensage);
+        messageView.setText(messageText);
+    }
+
+    public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+
+
+
+}
